1b57b6d23c93b33e2911fb626d76b4efb3d3fc1b
noelia <noeliatc.sys@gmail.com>
2022-07-27 09:58:10 -0700

    paquetes

---
+++ simulador-env/Scripts/pyre-upgrade-script.py
@@ -0,0 +1,12 @@
+#!C:\Users\Administrator\github\simulador\simulador-env\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'pyre-check==0.9.15','console_scripts','pyre-upgrade'
+__requires__ = 'pyre-check==0.9.15'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('pyre-check==0.9.15', 'console_scripts', 'pyre-upgrade')()
+    )
